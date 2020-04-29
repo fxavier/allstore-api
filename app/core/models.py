5e64ec24ff2d98d2feb5e470106db7bcb9b68125
Xavier Francisco <fxavier@Xaviers-MacBook-Pro.local>
2020-04-29 10:48:52 +0200

    Fixed discount_price bug

--- app/core/models.py
+++ app/core/models.py
@@ -60,6 +60,7 @@ class Category(models.Model):
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
